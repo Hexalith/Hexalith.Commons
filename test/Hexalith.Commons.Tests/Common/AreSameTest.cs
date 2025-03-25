422c1239e6466c2f3bc434b446feb3ca44a4b292
Jérôme Piquot <jpiquot@itaneo.com>
2025-03-25 15:13:12 +0100

    Update tests and enhance build script functionality
    - Updated the `Hexalith.Builds` subproject commit.
    - Added unit tests for the `AreSame` method in `AreSameTest.cs`, covering equatable and non-equatable scenarios.
    - Enhanced `builds-submodule-init.ps1` to create symbolic links and directories, with checks for existing files and warnings for missing sources.

--- test/Hexalith.Commons.Tests/Common/AreSameTest.cs
+++ test/Hexalith.Commons.Tests/Common/AreSameTest.cs
@@ -1,4 +1,5 @@
-﻿// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved. Licensed under the MIT license. See LICENSE file in the project root for full license information.
+﻿// Copyright (c) ITANEO (https://www.itaneo.com). All rights reserved. Licensed under the MIT
+// license. See LICENSE file in the project root for full license information.
 
 namespace Hexalith.Commons.Tests.Common;
 
@@ -6,8 +7,14 @@ using Hexalith.Commons.Objects;
 
 using Shouldly;
 
+/// <summary>
+/// Unit tests for the AreSame method in various dummy classes.
+/// </summary>
 public class AreSameTest
 {
+    /// <summary>
+    /// Tests that AreSame returns true for embedded equatable objects with the same properties.
+    /// </summary>
     [Fact]
     public void EmbeddedEquatableAreSameShouldReturnTrue()
     {
@@ -16,6 +23,9 @@ public class AreSameTest
         a.AreSame(b).ShouldBeTrue();
     }
 
+    /// <summary>
+    /// Tests that AreSame returns false for equatable objects with different properties.
+    /// </summary>
     [Fact]
     public void EquatableAreSameShouldReturnFalse()
     {
@@ -24,6 +34,9 @@ public class AreSameTest
         a.AreSame(b).ShouldBeFalse();
     }
 
+    /// <summary>
+    /// Tests that AreSame returns true for equatable objects with the same properties.
+    /// </summary>
     [Fact]
     public void EquatableAreSameShouldReturnTrue()
     {
@@ -32,6 +45,9 @@ public class AreSameTest
         a.AreSame(b).ShouldBeTrue();
     }
 
+    /// <summary>
+    /// Tests that AreSame returns false for non-equatable objects.
+    /// </summary>
     [Fact]
     public void NonEquatableAreSameShouldReturnFalse()
     {
@@ -40,6 +56,9 @@ public class AreSameTest
         a.AreSame(b).ShouldBeFalse();
     }
 
+    /// <summary>
+    /// Tests that AreSame returns true for the same non-equatable object.
+    /// </summary>
     [Fact]
     public void NonEquatableAreSameShouldReturnTrue()
     {
