42c6cb261f6629143b6b2c35f168fb23dfa0e384
Jérôme Piquot <jpiquot@itaneo.com>
2025-12-21 18:12:55 +0100

    fix: Rename namespace from Hexalith.Extensions.Helpers to Hexalith.Commons.Strings

--- src/libraries/Hexalith.Commons/Strings/StringHelper.cs
+++ src/libraries/Hexalith.Commons/Strings/StringHelper.cs
@@ -3,7 +3,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 // </copyright>
 
-namespace Hexalith.Extensions.Helpers;
+namespace Hexalith.Commons.Strings;
 
 using System.Globalization;
 using System.Text.RegularExpressions;
